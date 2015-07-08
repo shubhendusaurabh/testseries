5d74b77748e875443f10bac27652f6a7e298d39e
Shubhendu <shubhendu.saurabh@hotmail.com>
2015-07-09 00:00:30 +0530

    added question model

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+	PHYSICS = 'P'
+	CHEMISTRY = 'C'
+	MATHS = 'M'
+	Subject_Type_Choices = (
+		(PHYSICS, 'Physics'),
+		(CHEMISTRY, 'Chemistry'),
+		(MATHS, 'Maths'),
+	)
+	subject = models.CharField(max_length=1, choices=Subject_Type_Choices)
+	question_text = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
